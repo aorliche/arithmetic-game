5c9b5a190563651f408a5c734491f6d0bbe18149
Anton Orlichenko <aorliche@gmail.com>
2025-07-19 18:18:10 -0400

    Subproblems almost (all the way?) there

--- script/game.js
+++ script/game.js
@@ -1,11 +1,40 @@
 
 import {numToHunString} from './util.js';
-export {Game};
+export {Point, Game};
+
+function answerToNumber(arr) {
+	const answerCopy = arr.slice(0);
+	answerCopy.reverse();
+	let answer = 0;
+	let power = 1;
+	for (let i=0; i<answerCopy.length; i++) {
+		answer += power*answerCopy[i];
+		power *= 10;
+	}
+	return answer;
+}
+
+class Rect {
+	constructor(topLeft, dims) {
+		this.topLeft = topLeft;
+		this.dims = dims;
+	}
+
+	contains(p) {
+		const inx = p.x >= this.topLeft.x && p.x <= (this.topLeft.x + this.dims.x);
+		const iny = p.y >= this.topLeft.y && p.y <= (this.topLeft.y + this.dims.y);
+		return inx && iny;
+	}
+}
 
 class Point {
 	constructor(x, y) {
 		this.x = x;
 		this.y = y;
+	}
+
+	add(p) {
+		return new Point(this.x + p.x, this.y + p.y);
 	}
 
 	clone() {
@@ -19,6 +48,91 @@ class Point {
 	}
 }
 
+class Subproblem {
+	constructor(op, num1, num2, cloud, theta) {
+		if (['plus', 'minus', 'times'].indexOf(op) == -1) {
+			throw `Bad operation: ${op}`;
+		}
+		this.op = op;
+		this.num1 = num1;
+		this.num2 = num2;
+		this.text = `${numToHunString(this.num1)}*${numToHunString(this.num2)} = `;
+		this.cloud = cloud;
+		this.theta = theta;
+		this.dimensions = null;
+	}
+
+	get center() {
+		const dx = 100*Math.cos(this.theta);
+		const dy = 80*Math.sin(this.theta);
+		return this.cloud.center.add(new Point(dx, dy));
+	}
+
+	click(p) {
+		if (this.solved) {
+			return;
+		}
+		const rect = new Rect(
+			new Point(
+				this.center.x - this.dimensions.x/2, 
+				this.center.y - this.dimensions.y/2
+			),
+			new Point(
+				this.dimensions.x,
+				this.dimensions.y));
+		if (rect.contains(p)) {
+			return true;
+		}
+	}
+
+	draw(ctx) {
+		ctx.font = '38px HunimalSans';
+		ctx.fillStyle = '#000';
+		ctx.textBaseline = 'middle';
+		const stats = ctx.measureText(this.text);
+		if (this.dimensions == null) {
+			this.dimensions = new Point(stats.width+40, 40);
+		}
+		if (!this.solved && this.cloud.game.selected != this) {
+			ctx.strokeStyle = '#aaa';
+			ctx.lineWidth = 5;
+			ctx.lineJoin = 'bevel';
+			ctx.strokeRect(
+				this.center.x-this.dimensions.x/2, 
+				this.center.y-this.dimensions.y/2, 
+				this.dimensions.x,
+				this.dimensions.y);
+			ctx.fillStyle = '#33b';
+			ctx.fillRect(
+				this.center.x-this.dimensions.x/2, 
+				this.center.y-this.dimensions.y/2, 
+				this.dimensions.x,
+				this.dimensions.y);
+			ctx.fillStyle = '#33b';
+			ctx.fillStyle = "#fff";
+			ctx.font = "18px HunimalSans";
+			const stats2 = ctx.measureText("Subproblem");
+			ctx.fillText("Subproblem", this.center.x-stats2.width/2, this.center.y);
+		} else {
+			ctx.fillStyle = "#000";
+			ctx.font = "24px HunimalSans";
+			ctx.fillText(this.text, this.center.x-stats.width/2-20, this.center.y);
+			ctx.lineWidth = 2;
+			ctx.strokeRect(this.center.x+stats.width/2-60, this.center.y-20, 60, 40);
+			if (this.answer) {
+				ctx.fillStyle = '#f00';
+				if (this.solved) {
+					ctx.fillStyle = '#0c0';
+				}
+				ctx.fillText(
+					numToHunString(answerToNumber(this.answer)), 
+					this.center.x+stats.width/2-55, 
+					this.center.y);
+			}
+		}
+	}
+}
+
 class Cloud {
 	constructor(game, center) {
 		this.center = center.clone();
@@ -28,7 +142,23 @@ class Cloud {
 		this.colors = ['#cce', '#ddf', '#eef', '#fff', '#fff'];
 		this.makeParts();
 		this.makeQuestion();
-		this.answer = 0;
+		this.showSubproblems = true;
+		this.makeRoundedSubproblemParts();
+	}
+
+	click(p) {
+		if (this.center.dist(p) < 1.5*this.radius) {
+			for (let i=0; i<this.subproblems.length; i++) {
+				const inside = this.subproblems[i].click(p);
+				if (inside) {
+					this.game.selected = this.subproblems[i];
+					return true;
+				}
+			}
+			this.game.selected = this;
+			return true;
+		}
+		return false;
 	}
 
 	draw(ctx) {
@@ -51,10 +181,19 @@ class Cloud {
 		ctx.textBaseline = 'middle';
 		const stats = ctx.measureText(this.text);
 		ctx.fillText(this.text, this.center.x-stats.width/2-20, this.center.y);
+		ctx.strokeStyle = '#aaa';
+		ctx.lineWidth = 5;
+		ctx.lineJoin = 'bevel';
 		ctx.strokeRect(this.center.x+stats.width/2-20, this.center.y-30, 80, 60);
-		if (this.answer != 0) {
+		if (this.answer) {
 			ctx.fillStyle = '#f00';
-			ctx.fillText(numToHunString(this.answer), this.center.x+stats.width/2-15, this.center.y);
+			ctx.fillText(
+				numToHunString(answerToNumber(this.answer)), 
+				this.center.x+stats.width/2-15, 
+				this.center.y);
+		}
+		if (this.showSubproblems && this.subproblems.length > 0) {
+			this.subproblems.forEach(prob => prob.draw(ctx));
 		}
 	}
 
@@ -75,6 +214,30 @@ class Cloud {
 		this.num2 = Math.floor(99*Math.random())+1;
 		this.text = `${numToHunString(this.num1)}*${numToHunString(this.num2)} = `;
 		console.log('Answer: ' + (this.num1*this.num2));
+	}
+
+	makeRoundedSubproblemParts() {
+		let num1round = Math.round(this.num1/10)*10;
+		let num2round = Math.round(this.num2/10)*10;
+		let d1 = num1round === 0 ? 1000 : num1round-this.num1;
+		let d2 = num2round === 0 ? 1000 : num2round-this.num2;
+		let num1 = this.num1;
+		let num2 = this.num2;
+		this.subproblems = [];
+		const theta = Math.PI/4+Math.random()*Math.PI/2;
+		if (Math.abs(d1) > Math.abs(d2)) {
+			num1round = num2round;
+			d1 = d2;
+			num2 = num1;
+		}
+		if (d1 == 0) {
+			// Too easy
+			this.subproblems.push(new Subproblem('times', num1, num2round, this, theta));
+			this.subproblems.push(new Subproblem('times', num1, Math.abs(d2), this, theta+Math.PI));
+			return;
+		}
+		this.subproblems.push(new Subproblem('times', num1round, num2, this, theta));
+		this.subproblems.push(new Subproblem('times', Math.abs(d1), num2, this, theta+Math.PI));
 	}
 
 	tick() {
@@ -98,8 +261,6 @@ class Game {
 		this.score = 0;
 		this.attempted = 0;
 		this.speed = 0.1;
-		this.answer = [];
-		this.selectedCloud = null;
 	}
 
 	get width() {
@@ -137,50 +298,56 @@ class Game {
 			console.log('Failed to make cloud');
 		} else {
 			this.attempted += 1;
-			this.selectedCloud = this.clouds.at(-1);
 		}
 	}
 
 	addListeners() {
 		this.canvas.addEventListener('keydown', e => {
+			if (!this.selected) {
+				return;
+			}
+			if (!this.selected.answer) {
+				this.selected.answer = [];
+			}
 			for (let i=0; i<10; i++) {
 				if (e.key === i.toString()) {
-					this.answer.push(i);
+					this.selected.answer.push(i);
 				}
 			}
 			if (e.key === 'Backspace') {
-				this.answer.splice(this.answer.length-1, 1);
+				this.selected.answer.splice(this.selected.answer.length-1, 1);
 			}
-			// Check answer
-			const answerCopy = this.answer.slice(0);
-			answerCopy.reverse();
-			let answer = 0;
-			let power = 1;
-			for (let i=0; i<answerCopy.length; i++) {
-				answer += power*answerCopy[i];
-				power *= 10;
-			}
-			// Draw answer
-			this.selectedCloud.answer = answer;
 			// Check answer	
-			const trueAnswer = this.selectedCloud.num1 * this.selectedCloud.num2;
-			if (e.key === 'Enter') {
-				if (answer === trueAnswer) {
-					this.answer = [];
-					this.score += 1;
-					const keepClouds = [];
-					for (let i=0; i<this.clouds.length; i++) {
-						if (this.clouds[i] == this.selectedCloud) {
-							continue;
+			const trueAnswer = this.selected.num1 * this.selected.num2;
+			if (answerToNumber(this.selected.answer) === trueAnswer) {
+				if (this.selected instanceof Subproblem) {
+					this.selected.solved = true;
+					this.score += 0.3;
+				} else {
+					this.selected.subproblems.forEach(sub => {
+						if (sub.solved) {
+							this.score -= 0.3;
 						}
-						keepClouds.push(this.clouds[i]);
+					});
+					this.score += 1;
+				}
+				const keepClouds = [];
+				for (let i=0; i<this.clouds.length; i++) {
+					if (this.clouds[i] == this.selected) {
+						continue;
 					}
-					this.clouds = keepClouds;
-					if (this.clouds.length == 0) {
-						this.addCloud();
-					}
+					keepClouds.push(this.clouds[i]);
+				}
+				this.clouds = keepClouds;
+				if (this.clouds.length == 0) {
+					this.addCloud();
 				}
 			}
+		});
+		this.canvas.addEventListener('click', e => {
+			const p = new Point(e.offsetX, e.offsetY);
+			console.log(p);
+			this.clouds.forEach(c => c.click(p));
 		});
 	}
 
